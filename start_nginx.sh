73f4da5809c2ad799b74d25b758039534cbe2e06
Aus Abdalrasul <ausrasul@gmail.com>
2024-06-28 01:00:11 +0200

    nginx file uses domain name from conf.sh

--- start_nginx.sh
+++ start_nginx.sh
@@ -5,9 +5,16 @@ source "$(dirname "$0")/conf.sh"
 # Define the name of the Docker container
 CONTAINER_NAME="nginx_container"
 
+NGINX_CONFIG_SRC="$(dirname "$0")/nginx.conf.src"
+NGINX_CONFIG_COMPILED="$(dirname "$0")/nginx.conf"
+
 # Define the path to your local Nginx configuration file
 
 cp ${SSL_CERT_DIR}/live/*/* ${SSL_CERT_CURRENT}/
+
+# Process nginx.conf.template with environment variables
+export ${DOMAIN}
+envsubst '$DOMAIN' < ${NGINX_CONFIG_SRC} > ${NGINX_CONFIG_COMPILED}
 
 # Stop and remove the Docker container if it already exists
 if [ $(docker ps -a -f name=$CONTAINER_NAME | grep -w $CONTAINER_NAME | wc -l) -eq 1 ]; then
